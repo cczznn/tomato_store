3376ab22b20de3de6ba58ebe6cf43e22484b3daf
cczznn <1405754855@qq.com>
2025-04-03 03:29:48 +0800

    更改

--- src/main/java/com/example/tomatomall/repository/ProductRepository.java
+++ src/main/java/com/example/tomatomall/repository/ProductRepository.java
@@ -3,7 +3,13 @@ package com.example.tomatomall.repository;
 import com.example.tomatomall.po.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product,String>
 {
     Product findByTitle(String title);
+
+    Optional<Product> findById(Integer id);
+
+    void deleteById(Integer id);
 }
